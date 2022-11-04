d7aa559e4304402d52abbf34f71456d7c8d4ad13
Ishaq Nasir <ishaqnasir47@gmail.com>
2022-11-04 17:23:18 +0100

    update

---
+++ components/post.js
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import styles from "../styles/Home.module.css";
+
+export function Post() {
+  return (
+    <form action="api/form" method="post">
+      <label htmlFor="body-content">Body</label>
+      <input type="text" id="body-content" name="body-content" required />
+
+      <button type="submit">Post</button>
+    </form>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,14 @@ import Head from "next/head";
 import Header from "../components/Header";
 import styles from "../styles/Home.module.css";
 import { useSession } from "next-auth/react";
+import { ImageUpload } from "../components/uploadWidget";
+import { Post } from "../components/post";
 
 export default function Home() {
   const { data: session, status } = useSession();
   const loading = status === "loading";
+  // const [url, updateUrl] = useState();
+  // const [error, updateError] = useState();
 
   return (
     <div className={styles.container}>
@@ -23,13 +27,19 @@ export default function Home() {
                 You're logged in as, {session.user.name ?? session.user.email}!
               </p>
               <p style={{ marginBottom: "10px" }}> </p> <br />
-              <img src="profile-pic.jpg" alt="" className={styles.avatar} />
+              {/* <img src="" alt="" className={styles.avatar} />  */}
+              <div className={styles.grid}>
+                <ImageUpload />
+              </div>
+              <div className="">
+                <Post />
+              </div>
             </>
           )}
           {!session && (
             <>
-              <p className={styles.title}>Please login and flaunt</p>
-              <img src="" alt="" className={styles.avatar} />
+              <p className={styles.title}>SIGN IN TO FLAUNT</p>
+              {/* <img src="" alt="" className={styles.avatar} /> */}
             </>
           )}
         </div>
@@ -38,27 +48,27 @@ export default function Home() {
   );
 }
 
-export const getServerSideProps = async ({ req, res }) => {
-  const { isAuthenticated, username } = await authorize(req);
+// export const getServerSideProps = async ({ req, res }) => {
+//   const { isAuthenticated, username } = await authorize(req);
 
-  if (isAuthenticated && username) {
-    const xata = getXataClient();
+//   if (isAuthenticated && username) {
+//     const xata = getXataClient();
 
-    const todos = await xata.db.items
-      .filter("user.username", username)
-      .getMany();
+//     const todos = await xata.db.items
+//       .filter("user.username", username)
+//       .getMany();
 
-    return {
-      props: {
-        todos,
-      },
-    };
-  } else {
-    res.writeHead(401, {
-      "WWW-Authenticate": "Basic realm='This is a private to-do list'",
-    });
+//     return {
+//       props: {
+//         todos,
+//       },
+//     };
+//   } else {
+//     res.writeHead(401, {
+//       "WWW-Authenticate": "Basic realm='This is a private to-do list'",
+//     });
 
-    res.end();
-    return {};
-  }
-};
+//     res.end();
+//     return {};
+//   }
+// };
